613a41743ca7ca8f3ae502f1147f0594935802c3
Pontus N <pontusnorr@gmail.com>
2022-12-03 14:58:36 +0100

    cleanup

--- day3/script.py
+++ day3/script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 a = __import__("string").ascii_letters
-d = [x.strip() for x in __import__("sys").stdin.readlines()]
+d = __import__("sys").stdin.read().split("\n")[:-1]
 print("star1:{}".format(sum([a.index((set(l[:len(l)//2]) & set(l[len(l)//2:])).pop())+1 for l in d])))
 print("star2:{}".format(sum([a.index((x&y&z).pop())+1 for(x,y,z)in(map(set,d[i:i+3])for i in range(0,len(d),3))])))
