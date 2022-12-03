cb32e941caf18d12a56e984bda1dad69f6a040b9
Pontus N <pontusnorr@gmail.com>
2022-12-03 14:46:39 +0100

    progress

--- day3/script.py
+++ day3/script.py
@@ -4,4 +4,5 @@ a = string.ascii_letters
 d = [x.strip() for x in sys.stdin.readlines()]
 print("star1:{}".format(sum([a.index((set(l[:len(l)//2]) & set(l[len(l)//2:])).pop())+1 for l in d])))
 print("star2:{}".format(sum([a.index(list(set.intersection(*map(set,d[i:i+3])))[0])+1 for i in range(0,len(d),3)])))
+print("star2:{}".format([a.index((x&y&z).pop())+1 for (x,y,z) in (map(set,d[i:i+3]) for i in range(0,len(d),3))]))
 
